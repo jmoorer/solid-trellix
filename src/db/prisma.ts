52ba0d06f4dff7710b431c7ac69e211482c61afa
Marlon <marlonmoorer@outlook.com>
2024-03-09 13:58:15 -0500

    log env

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
+console.log({ env: process.env });
 process.on("beforeExit", () => {
   prisma.$disconnect();
 });
