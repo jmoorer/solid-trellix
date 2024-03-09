e801c4d7b745d27bbb7ff534f6939ea7bf908319
Marlon <marlonmoorer@outlook.com>
2024-03-09 12:48:23 -0500

    removed server preset

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
-  server: {
-    preset: "node",
-  },
   vite: {
     plugins: [
       svgLoader({
